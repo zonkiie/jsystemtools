0ee1a4b9542e39217c1428cbca1bea7399ce42d4
Rainer Datzer <rdatz@gmx.de>
2022-07-03 02:08:23 +0200

    added grouphandler

---
+++ src/handlers/GroupHandler.java
@@ -0,0 +1,18 @@
+package handlers;
+
+import interfaces.*;
+import entities.*;
+import java.lang.*;
+import java.util.*;
+import java.util.regex.*;
+
+@PublicCallable
+public class GroupHandler implements CRUDLS<UnixGroup>, Handler
+{
+	@PublicCallable
+	public String info()
+	{
+		return "Handles Group Management";
+	}
+	
+}
