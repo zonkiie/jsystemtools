c676929bd0266c76929797fb49a3348e058ad5f3
Rainer Datzer <rdatz@gmx.de>
2022-08-04 03:19:39 +0200

    improved shadow handler

--- doc/Links.md
+++ doc/Links.md
@@ -6,3 +6,4 @@ https://www.baeldung.com/java-find-all-classes-in-package
 https://stackoverflow.com/questions/16981232/is-there-an-equivalent-method-to-cs-scanf-in-java
 https://stackoverflow.com/questions/15588903/get-group-names-in-java-regex
 https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
--- src/handlers/ShadowHandler.java
+++ src/handlers/ShadowHandler.java
@@ -5,6 +5,7 @@ import entities.*;
 import java.lang.*;
 import java.util.*;
 import java.util.regex.*;
+import java.security.*;
 
 @PublicCallable
 public class ShadowHandler implements CRUDLS<UnixShadow>, Handler
@@ -20,6 +21,213 @@ public class ShadowHandler implements CRUDLS<UnixShadow>, Handler
 		return this;
 	}
 	
+	public CRUDLS<UnixShadow> add(List<UnixShadow> o)
+	{
+		return this;
+	}
 	
+	public CRUDLS<UnixShadow> set(UnixShadow o)
+	{
+		try
+		{
+			List<String> Params = new ArrayList<String>(){{add("usermod");}};
+			if(o.newPassword != null)
+			{
+				o.sp_pwdp = getSecurePassword(o.newPassword, getSalt());
+				Params.add("-p");
+				Params.add(o.sp_pwdp);
+			}
+			if(o.sp_expire != null)
+			{
+				Params.add("-e");
+				Params.add(o.sp_expire);
+			}
+			if(o.sp_inact != null)
+			{
+				Params.add("-i");
+				Params.add(o.sp_inact);
+			}
+			Params.add(o.pw_name);
+			String[] arrayParams = Params.toArray(new String[Params.size()]);
+			ExecutorReturn executorReturn = SimpleExecutor.execute(arrayParams);
+			if(executorReturn.returnCode != 0) throw new Exception(executorReturn.toString());
+			return this;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public CRUDLS<UnixShadow> set(List<UnixShadow> o)
+	{
+		for(UnixShadow entry: o) set(entry);
+		return this;
+	}
+	
+	public CRUDLS<UnixShadow> rename(String from, String to)
+	{
+		try
+		{
+			return this;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	public CRUDLS<UnixShadow> rename(UnixShadow oldObject, UnixShadow renamedObject)
+	{
+		try
+		{
+			rename(oldObject.pw_name, renamedObject.pw_name);
+			return this;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	@PublicCallable
+	public UnixShadow get(String id)
+	{
+		try
+		{
+			ExecutorReturn executorReturn = SimpleExecutor.execute("getent", "shadow", id);
+			if(executorReturn.returnCode != 0) throw new Exception("Something failed! Return code:" + executorReturn.returnCode);
+			UnixPasswd entry = parseLine(executorReturn.toString());
+			return entry;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public UnixShadow get(UnixShadow o)
+	{
+		return get(o.pw_name);
+	}
+	
+	@PublicCallable
+	public List<UnixShadow> list()
+	{
+		try
+		{
+			List<UnixShadow> entryList = new ArrayList<UnixShadow>();
+			return entryList;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public List<UnixShadow> search(UnixShadow o)
+	{
+		try
+		{
+			List<UnixShadow> entryList = new ArrayList<UnixShadow>();;
+			return entryList;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public CRUDLS<UnixShadow> delete(UnixShadow o)
+	{
+		return this;
+	}
+	
+	public CRUDLS<UnixShadow> delete(List<UnixShadow> userList)
+	{
+		return this;
+	}
+	
+	public CRUDLS<UnixShadow> delete(String id)
+	{
+		try
+		{
+			return this;
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public UnixShadow parseLine(String line)
+	{
+		UnixShadow entry = new UnixShadow();
+		String [] els = line.split(":");
+		if(els.length != 9) return null; //Something failed. 9 Entries are nesessary.
+		entry.sp_namp = els[0];
+		entry.sp_pwdp = els[1];
+		entry.sp_lstchg = Integer.valueOf(els[2]);
+		entry.sp_min = Integer.valueOf(els[3]);
+		entry.sp_max = Integer.valueOf(els[4]);
+		entry.sp_warn = Integer.valueOf(els[5]);
+		entry.sp_inact = Integer.valueOf(els[6]);
+		entry.sp_expire = Integer.valueOf(els[7]);
+		entry.sp_flag = Integer.valueOf(els[8]);
+		return entry;
+	}
+	
+	private static String getSecurePassword(String passwordToHash, String salt) {
+		String generatedPassword = null;
+		try {
+			// Create MessageDigest instance for MD5
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			
+			// Add password bytes to digest
+			md.update(salt.getBytes());
+			
+			// Get the hash's bytes
+			byte[] bytes = md.digest(passwordToHash.getBytes());
+			
+			// This bytes[] has bytes in decimal format;
+			// Convert it to hexadecimal format
+			StringBuilder sb = new StringBuilder();
+			
+			for (int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
+						.substring(1));
+			}
+			
+			// Get complete hashed password in hex format
+			generatedPassword = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return generatedPassword;
+	}
+
+	
+	// Add salt
+	private static String getSalt()
+			throws NoSuchAlgorithmException, NoSuchProviderException 
+	{
+		// Always use a SecureRandom generator
+		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+
+		// Create array for salt
+		byte[] salt = new byte[16];
+
+		// Get a random salt
+		sr.nextBytes(salt);
+
+		// return salt
+		return salt.toString();
+	}
 	
 }
